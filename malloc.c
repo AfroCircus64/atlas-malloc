3fc62fde795509f2d90efd7bfe646c6f5961f26a
Trey Turner <trey.turner1857@gmail.com>
2025-05-26 01:25:37 -0500

    Pushing what I have. (will revise tomorrow)

--- malloc.c
+++ malloc.c
@@ -1,5 +1,27 @@
 #include "malloc.h"
 
+static m_header_t *head;
+
+/**
+ * find_free_block - Finds a free block of sufficient size.
+ *
+ * @size: The size needed.
+ *
+ * Return: Pointer to a suitable block or NULL.
+ */
+static m_header_t *find_free_block(size_t size)
+{
+	m_header_t *curr = head;
+
+	while (curr)
+	{
+		if (curr->free && curr->size >= size)
+			return (curr);
+		curr = curr->next;
+	}
+	return (NULL);
+}
+
 /**
  * _malloc - Allocates memory of a given size.
  *
@@ -9,5 +31,32 @@
  */
 void *_malloc(size_t size)
 {
-    /* Code goes here */
+	m_header_t *block;
+	void *mem;
+	size_t total_size;
+
+	if (size == 0)
+		return (NULL);
+
+	size = ALIGN4(size);
+	block = find_free_block(size);
+
+	if (block)
+	{
+		block->free = 0;
+		return ((void *)(block + 1));
+	}
+
+	total_size = sizeof(m_header_t) + size;
+	mem = sbrk(total_size);
+	if (mem == (void *)-1)
+		return (NULL);
+
+	block = (m_header_t *)mem;
+	block->size = size;
+	block->free = 0;
+	block->next = head;
+	head = block;
+
+	return ((void *)(block + 1));
 }
--- malloc.h
+++ malloc.h
@@ -2,8 +2,21 @@
 #define MALLOC_H
 #include <stdio.h>
 
-
 /* STRUCTURES */
+
+/**
+ * struct m_header_s - Structure representing a block of memory in the heap.
+ * @size: The size of the block.
+ * @free: A flag indicating whether the block is free (1) or allocated (0).
+ * @next: Pointer to the next block in the linked list.
+ */
+typedef struct m_header_s
+{
+	size_t size;
+	int free;
+	struct m_header_s *next;
+} m_header_t;
+
 
 
 /* FUNCTION PROTOTYPES */
